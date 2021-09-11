75daebef35d25967e39cdb3b84cd7cd1c7574bc9
jifrozen <answldjs1836@naver.com>
2021-09-11 23:47:36 +0900

    [Feat] Chat with WebSockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
-hello;
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('form');
+const socket = new WebSocket(`ws://${window.location.host}`);
+function handleOpen() {
+  console.log('Connected to Server');
+}
+
+socket.addEventListener('open', handleOpen);
+
+socket.addEventListener('message', (message) => {
+  console.log('Disconnected from Server');
+});
+
+function handelSubmit(event) {
+  event.preventDefault();
+  const input = messageFrom.querySelector('input');
+  socket.send(input.value);
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
+import http from 'http';
 import express from 'express';
+import WebSocket from 'ws';
 import path from 'path';
+
 const __dirname = path.resolve();
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/src/views');
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClase() {
+  console.lo('Disconnected from the Browser');
+}
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket.on('close', onSocketClose);
+  socket.on('message', (message) => {
+    sockets.forEach((asocket) => asocket.send(message));
+  });
+});
+
+server.handleListen(3000, handleListen);
