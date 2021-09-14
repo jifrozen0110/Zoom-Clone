7ddc2a4724cc038be2e334d7ddc1236a2f76d338
jifrozen <answldjs1836@naver.com>
2021-09-14 18:55:45 +0900

    [Feat] Video Call - call controls(mute, camera on&off)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,57 @@ const socket = io();
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
 const room = document.getElementById('room');
+const myFace=document.getElementById("myFace");
+const mute=document.getElementById("mute");
+const camera=document.getElementById("camera");
+const camerasSelect=document.getElementById("cameras")
+const call=document.getElementById("call")
 
-room.hidden = true;
+call.hidden=true
 
+let myStream;
+let muted=false;
+let cameraOff=false;
 let roomName;
+
+async function getCameras(){
+  try{
+    const devices=await navigator.mediaDevices.enumerateDevices();
+    const cameras=devices.filter((device) => device.kind =="videoinput")
+    const currentCamera=myStream.getVideoTracks()[0]
+    cameras.forEach((camera)=>{
+      const option=document.createElement("option")
+      option.value=camera.deviceId
+      option.innerText=camera.label;
+      if(currentCamera.label==camera.label){
+        option.selected=true;
+      }
+    })
+  }catch(e){
+    console.log(e)
+  }
+}
+async function getMedia(deviceId){
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try{
+    myStream=await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    )
+    myFace.srcObject=myStream;
+    if(!deviceId){
+    await getCameras()
+    }
+  }catch(e){
+    console.log(e);
+  }
+}
 
 function addMessage(message){
   const ul=room.querySelector("ul");
@@ -27,7 +74,8 @@ function handleMessageSubmit(event){
 
 function showRoom() {
   welcome.hidden = true;
-  room.hidden = false;
+  call.hidden=false
+  getMedia()
   const h3 = room.querySelector('h3');
   h3.innerText = `Room ${roomName}`;
   const form =room.querySelector("form")
@@ -43,7 +91,38 @@ function handleRoomSubmit(event) {
   roomNameInput.value = '';
 }
 
+function handleMuteClick(){
+  myStream.getAudioTracks().forEach((track)=>(track.enabled=!track.enabled))
+  if(!muted){
+    mute.innerText="Unmute"
+    muted=true
+  }
+  else{
+    mute.innerText="mute"
+    muted=false;
+  }
+}
+
+function handleCameraClick(){
+  myStream.getVideoTracks().forEach((track)=>(track.enabled=!track.enabled))
+  if(!cameraOff){
+    camera.innerText="Trun Camera On"
+    cameraOff=true
+  }
+  else{
+    camera.innerText="Trun Camera Off"
+    cameraOff=false;
+  }
+}
+
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 form.addEventListener("submit",handleRoomSubmit)
+mute.addEventListener("click",handleMuteClick)
+camera.addEventListener("click",handleCameraClick)
+camerasSelect.addEventListener("input", handleCameraChange);
 
 socket.on("welcome",(user,newCount)=>{
   const h3 = room.querySelector("h3");
@@ -67,3 +146,4 @@ socket.on("room_change",(rooms)=>{
     roomList.append(li)
   })
 })
+
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/src/views');
 app.use('/public', express.static(__dirname + '/src/public'));
 app.get('/', (_, res) => res.render('home'));
+app.get('/video', (_, res) => res.render('video'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const server = http.createServer(app);
